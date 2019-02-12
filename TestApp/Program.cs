acc03e41e76f8d2e41ef1cd684b13a7e3cf7d1cb
Stanislav <stanley.m.milton@gmail.com>
2019-02-12 13:54:39 +0300

    db connect

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace TestApp
+﻿
+namespace TestApp
 {
+    using GRKAdminCore;
+    using System;
+    using System.ComponentModel;
+
     internal class Program
     {
         public static void Main(string[] args)
         {
+            using (var context = new LDEntities())
+            {
+                LDTABLE t = new LDTABLE()
+                {
+                    ID=1059,
+                    Name = "testtable_entity_2",
+                    CardTable = "-",
+                    GUIView = "+",
+                    Type = "U",
+                    DisplayLabel = "Тестовая таблица EF",
+                    Deleted = "-"
+                };
+                context.LDTABLE.Add(t);
+                context.SaveChanges();
+            }
+            
         }
+    }
+
+    public class TestTableDF
+    {
+        public int ID { get; set; }
+        [DisplayName("First Name")]
+        public string Fname { get; set; }
+        [DisplayName("Surname")]
+        public string Sname { get; set; }
     }
 }
---
+++ TestApp/TestInterceptor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace TestApp
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure.Interception;
+    using System.Data.Common;
+
+    class TestInterceptor : IDbCommandInterceptor
+    {
+        public void NonQueryExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            List<string> par = new List<string>();
+            DbParameterCollection p = command.Parameters;
+            DbParameter[] pars = new DbParameter[7];
+            p.CopyTo(pars, 0);
+
+            string commandText = command.CommandText;
+            List<DbParameter> prs = pars.ToList();
+            foreach (var x in prs)
+            {
+                string name = x.ParameterName.ToString();
+                string value;
+                if (x.DbType==System.Data.DbType.Int32)
+                    value = x.Value.ToString();
+                else
+                    value=$"'{x.Value.ToString()}'";
+                commandText = commandText.Replace(name, value);
+            }
+
+            LogInfo("NonQueryExecuted", commandText);
+        }
+
+        public void NonQueryExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            //LogInfo("NonQueryExecuting", String.Format(" IsAsync: {0}, Command Text: {1}", interceptionContext.IsAsync, command.CommandText));
+        }
+
+        public void ReaderExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<System.Data.Common.DbDataReader> interceptionContext)
+        {
+           // LogInfo("ReaderExecuted", String.Format(" IsAsync: {0}, Command Text: {1}", interceptionContext.IsAsync, command.CommandText));
+        }
+
+        public void ReaderExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<System.Data.Common.DbDataReader> interceptionContext)
+        {
+            //LogInfo("ReaderExecuting", String.Format(" IsAsync: {0}, Command Text: {1}", interceptionContext.IsAsync, command.CommandText));
+        }
+
+        public void ScalarExecuted(System.Data.Common.DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+           // LogInfo("ScalarExecuted", String.Format(" IsAsync: {0}, Command Text: {1}", interceptionContext.IsAsync, command.CommandText));
+        }
+
+        public void ScalarExecuting(System.Data.Common.DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            //LogInfo("ScalarExecuting", String.Format(" IsAsync: {0}, Command Text: {1}", interceptionContext.IsAsync, command.CommandText));
+        }
+
+        private void LogInfo(string command, string commandText)
+        {
+            Console.WriteLine(commandText);
+        }
+    }
+}
